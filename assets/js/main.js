54b64ed28c004029f59b8e91c69aac950692a810
JellyBitz <engelsquintero@gmail.com>
2020-02-03 06:53:56 -0430

    Added Area levels as navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,41 @@
-$(".sidebar-dropdown > a").click(function() {
-  $(".sidebar-submenu").slideUp(200);
-  if (
-    $(this)
-      .parent()
-      .hasClass("active")
-  ) {
-    $(".sidebar-dropdown").removeClass("active");
-    $(this)
-      .parent()
-      .removeClass("active");
-  } else {
-    $(".sidebar-dropdown").removeClass("active");
-    $(this)
-      .next(".sidebar-submenu")
-      .slideDown(200);
-    $(this)
-      .parent()
-      .addClass("active");
-  }
+$(".sidebar-dropdown > a").click(function()
+{
+	$(".sidebar-submenu").slideUp(200);
+	if ( $(this).parent().hasClass("active") )
+	{
+		$(".sidebar-dropdown").removeClass("active");
+		$(this).parent().removeClass("active");
+	} 
+	else
+	{
+		$(".sidebar-dropdown").removeClass("active");
+		$(this).next(".sidebar-submenu").slideDown(200);
+		$(this).parent().addClass("active");
+	}
 });
 
+$(".sidebar-submenu-dropdown > a").click(function()
+{
+	$(".sidebar-submenu-submenu").slideUp(200);
+	if ( $(this).parent().hasClass("active") )
+	{
+		$(".sidebar-submenu-dropdown").removeClass("active");
+		$(this).parent().removeClass("active");
+	} 
+	else
+	{
+		$(".sidebar-submenu-dropdown").removeClass("active");
+		$(this).next(".sidebar-submenu-submenu").slideDown(200);
+		$(this).parent().addClass("active");
+	}
+});
+
+
 $("#close-sidebar").click(function() {
-  $(".page-wrapper").removeClass("toggled");
+	$(".page-wrapper").removeClass("toggled");
 });
 $("#show-sidebar").click(function() {
-  $(".page-wrapper").addClass("toggled");
+	$(".page-wrapper").addClass("toggled");
 });
 
 xSROMap.init('map');
